463808d354bda5b7568cb95fba477c19b1633047
Linnea <linnea.alse@elev.ga.ntig.se>
2022-01-17 08:56:35 +0100

    color

--- tamagotchi/Program.cs
+++ tamagotchi/Program.cs
@@ -8,39 +8,53 @@ namespace tamagotchi
     {
         static void Main(string[] args)
         {
-             Raylib.InitWindow(800,600, "Tamagotchi"); //Initilization 
+            Raylib.InitWindow(800, 600, "Tamagotchi"); //Initilization 
 
-             string currentScreen = "title"; //adding a sort of function to switch screens
+            string currentScreen = "title"; //adding a sort of function to switch screens
 
-             List<string> tamagotchi  = new List<string>();
+            List<string> tamagotchi = new List<string>();
 
 
             Raylib.SetTargetFPS(60);
-                    Tamagotchi t1 = null;
+            Tamagotchi t1 = null;
 
-            while(!Raylib.WindowShouldClose()){ //Main game loop
 
-                List<string> tamagotchisOWned  = new List<string>();
 
-                switch(currentScreen){ //Game logic
+
+            while (!Raylib.WindowShouldClose())
+            { //Main game loop
+
+                List<string> tamagotchisOWned = new List<string>();
+
+                switch (currentScreen)
+                { //Game logic
                     case "title":
 
-                    if(Raylib.IsKeyPressed(KeyboardKey.KEY_ENTER)){
-                        if(tamagotchisOWned.Count < 1){
-                            currentScreen = "createAPet";
+                        if (Raylib.IsKeyPressed(KeyboardKey.KEY_ENTER))
+                        {
+                            if (tamagotchisOWned.Count < 1)
+                            {
+                                currentScreen = "createAPet";
+                                t1 = new Tamagotchi();
+
+                            }
+                            else
+                            {
+                                currentScreen = "petOwned";
+                            }
                         }
-                        else{
-                            currentScreen = "petOwned";
-                        }
-                    }
-                    
-                    break;
+
+                        break;
 
                     case "createAPet":
-                    
+                        if(Raylib.IsKeyPressed(KeyboardKey.KEY_ENTER)){
+                            currentScreen ="petScreen";
+                       
+                        }
 
 
-                    break;
+
+                        break;
 
 
                 }
@@ -48,26 +62,39 @@ namespace tamagotchi
                 Raylib.BeginDrawing();
                 Raylib.ClearBackground(Color.RAYWHITE);
 
-                switch(currentScreen){ //Drawing on that screen
+                switch (currentScreen)
+                { //Drawing on that screen
                     case "title":
-                    //Draw title
+                        //Draw title
 
-                    Raylib.DrawText("PRESS ENTER TO BEGIN GAME", 20, 20, 40, Color.LIGHTGRAY);
+                        Raylib.DrawText("PRESS ENTER TO BEGIN GAME", 20, 20, 40, Color.LIGHTGRAY);
 
-                    break;
+                        break;
 
-                    case "createAPet":
+                    case "createAPet": //make colour pick a switch statement inside createAPet
 
-                    if (t1 == null)
-                    {
-                        t1 = new Tamagotchi();
-                    }
-                    //customization features added here! 
+                    Raylib.DrawText("PRESS ENTER TO CONFIRM COLOUR", 20, 20, 40, Color.LIGHTGRAY);
+                    Raylib.DrawText("RIGHT TO TOGGLE BETWEEN COLOUR", 20, 70, 20, Color.LIGHTGRAY);
+
+                        t1.Draw();
+
+
+                        t1.PickColourOfPet();
+                        
+
+
+                        break;
+
+
+                    case "petScreen":
+
+
+                    t1.Draw();
+
                     
-                    t1.PickColourOfPet();
 
-                  
                     break;
+
 
                 }
 
--- tamagotchi/tamagotchi.cs
+++ tamagotchi/tamagotchi.cs
@@ -3,31 +3,66 @@ using System.Collections.Generic;
 using Raylib_cs;
 
 
-class Tamagotchi{
+class Tamagotchi
+{
 
-    public int x= 100;
-    public int y= 100; 
 
-    public int size = 300;
 
-    public Color colourOfPet = Color.BEIGE;
+    private int indexOfColour = 0;
+    public const int x = 100;
+    public const int y = 100;
 
-    public Tamagotchi(){
+    public const int size = 300;
 
-        Rectangle r1 = new Rectangle (x, y,size,size);
-       Raylib.DrawRectangleRec(r1, colourOfPet);
+    private Color[] colourOption = { Color.RED, Color.BLUE, Color.GREEN, Color.GRAY };
+
+    public Color colourOfPet = Color.BLANK;
+
+    private Rectangle r1 = new Rectangle(x, y, size, size);
+    public Tamagotchi()
+    {
+
+        // Raylib.ClearBackground(Color.RAYWHITE);
+
+
+        colourOfPet = colourOption[0];
+
+        PickColourOfPet();
+
+        Draw();
+
+        // Raylib.DrawRectangleRec(r1, colourOfPet);
+
+    }
+
+    public void Draw()
+    {
+        Raylib.DrawRectangleRec(r1, colourOfPet);
     }
 
 
-    public void PickColourOfPet(){
-        if(Raylib.IsKeyPressed(KeyboardKey.KEY_LEFT)){
-            colourOfPet = Color.BLACK;
+    public void PickColourOfPet()
+    {
 
 
+        if (Raylib.IsKeyPressed(KeyboardKey.KEY_RIGHT))
+        {
+            colourOfPet = colourOption[indexOfColour];
+
+            indexOfColour++;
+            System.Console.WriteLine(indexOfColour);
+
+            if (indexOfColour == colourOption.Length)
+            {
+                indexOfColour = 0;
+            }
         }
 
-    }
-  
 
- 
+
+
+    }
+
+
+
 }
